1e69e4f6d59b7d3af57492ac6a4b29d01dec6ca8
Mike Odom <5105729+mike-odom@users.noreply.github.com>
2020-01-21 09:38:55 -0800

    Removing build files

--- server/build/server/src/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class moo {
-}
-exports.moo = moo;
-//# sourceMappingURL=test.js.map
--- server/build/shared/src/Utils.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class moo {
-}
-//# sourceMappingURL=Utils.js.map
