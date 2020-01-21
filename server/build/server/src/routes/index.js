e33863898aae19b6692c843f83af8597b347ff3c
Mike Odom <5105729+ThatOdieGuy@users.noreply.github.com>
2020-01-21 09:38:55 -0800

    Removing build files

--- server/build/server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Index page' });
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
